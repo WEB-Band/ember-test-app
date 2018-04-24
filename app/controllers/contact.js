5f0248e0c50be8ccd71dce6612b3b2883ad9a433
imsaint <imsaint7s@gmail.com>
2018-04-24 16:47:09 +0300

    EL-31 Create feedback form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -3,4 +3,9 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     queryParams: ['sidebar'],
     sidebar: true,
+    actions: {
+        showAlert() {
+          alert('Congratulations! Data have been sent!');
+        }
+      }
 });
